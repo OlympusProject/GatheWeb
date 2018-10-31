863fc37ea64d8dc396bd9813630513a335697d77
citra puspita rahman <citrapuspita.r@gmail.com>
2018-10-31 22:46:21 +0700

    Add Side navigation bar

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class UserProfileComponent {
+  public openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+//document.getElementById("main").style.marginLeft = "250px";
+}
+public closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
 
 }
+
